1e21a7950e1f7f8a2b5d77eb4858fa563c34b238
Aman Lal <alal09635@gmail.com>
2026-06-30 17:05:02 -0700

    Add: is-palindrome - submission-4

---
+++ Data Structures & Algorithms/is-palindrome/submission-4.py
@@ -0,0 +1,6 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        clean_s = "".join(char for char in s if char.isalnum())
+        r_clean_s = "".join(reversed(clean_s))
+
+        return clean_s.upper() == r_clean_s.upper()
